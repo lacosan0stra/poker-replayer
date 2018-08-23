33c82547eb0d26633595021d249533fbff1493a0
Pablo <paul.ladiv123@gmail.com>
2018-08-23 13:55:19 +0100

    update PlayerDecision component

--- src/components/PlayerDecision.js
+++ src/components/PlayerDecision.js
@@ -3,23 +3,25 @@ import React, { Component } from 'react'
 class PlayerDecision extends Component {
     render() {
         return (
-            <div className='flex-item'>
-                {this.props.position.toUpperCase()}  PLAYER OPTIONS
-                <div className='flex-container'>
-                    <div className='flex-item'>
-                        BET
-                    </div>
-                    <div className='flex-item'>
-                        CHECK
-                    </div>
-                    <div className='flex-item'>
-                        CALL
-                    </div>
-                    <div className='flex-item'>
-                        RAISE
-                    </div>
-                    <div className='flex-item'>
-                        FOLD
+            <div>
+                <div className='flex-item'>
+                    {this.props.position.toUpperCase()}  PLAYER OPTIONS
+                    <div className='flex-container'>
+                        <div className='flex-item'>
+                            BET
+                        </div>
+                        <div className='flex-item'>
+                            CHECK
+                        </div>
+                        <div className='flex-item'>
+                            CALL
+                        </div>
+                        <div className='flex-item'>
+                            RAISE
+                        </div>
+                        <div className='flex-item'>
+                            FOLD
+                        </div>
                     </div>
                 </div>
             </div>
