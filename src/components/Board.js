18530ca7d967fb61f63e4a0d74a90b8d57bb127e
Pablo <paul.ladiv123@gmail.com>
2018-08-24 10:09:25 +0100

    turn hardcoded table into a loop-based built table

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import Card from "./Card";
+var context = require.context('../1x', true, /\.(png)$/);
+var classNames = {};
+var files={};
 
+const handValues = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'  ]
+const objHands = {}
+
+handValues.forEach((item, index)=>{
+    objHands[index +1] = handValues[index]
+})
+
+context.keys().forEach((filename)=>{
+    classNames[filename] = 'flex-item';
+    files[filename] = context(filename);
+});
 
 class Board extends Component {
 
+
+
     render() {
+        const objBroadwayHands ={
+            11 : 'jack',
+            12 : 'queen',
+            13 : 'king'
+        }
+
+        const suits = ["_heart.png", "_spade.png", "_club.png", "_diamond.png" ]
 
         return(
                 <div className='flex-container flex-item'>
@@ -21,92 +44,27 @@ class Board extends Component {
                                     <th className="suit redSuit" align="center">DIAM'S</th>
                                 </tr>
                                 <tr>
-                                    <td > <Card id="1_heart.png" textValue='Ace' /></td>
-                                    <td > <Card id="1_spade.png" textValue='Ace'/></td>
-                                    <td > <Card id="1_club.png" textValue='Ace'/></td>
-                                    <td > <Card id="1_diamond.png" textValue='Ace'/></td>
-                                </tr>
-                                <tr>
-                                    <td > <Card id="king_heart.png" textValue='King' /></td>
-                                    <td > <Card id="king_spade.png" textValue='King'/></td>
-                                    <td > <Card id="king_club.png" textValue='King'/></td>
-                                    <td > <Card id="king_diamond.png" textValue='King'/></td>
-                                </tr>
-                                <tr>
-                                    <td > <Card id="queen_heart.png" textValue='Queen' /></td>
-                                    <td > <Card id="queen_spade.png" textValue='Queen'/></td>
-                                    <td > <Card id="queen_club.png" textValue='Queen'/></td>
-                                    <td > <Card id="queen_diamond.png" textValue='Queen'/></td>
+                                    {
+                                        suits.map((suit)=> {
+                                            return (
+                                                <td>
+                                                    {Object.keys(objHands).map((key,value) => {
+                                                        let idKey
+                                                        if ( key < 11) {
+                                                            idKey = key + suit
+                                                        } else {
+                                                            idKey = objBroadwayHands[key] + suit
+                                                        }
+                                                        const cardName = objHands[key]
+                                                        return ( <Card id={idKey} textValue={cardName}/>)
 
-                                </tr>
-                                <tr>
-                                    <td > <Card id="jack_heart.png" textValue='Jack' /></td>
-                                    <td > <Card id="jack_spade.png"textValue='Jack'/></td>
-                                    <td > <Card id="jack_club.png" textValue='Jack'/></td>
-                                    <td > <Card id="jack_diamond.png"textValue='Jack'/></td>
+                                                    })
+                                                    }
 
-                                </tr>
-                                <tr>
-                                    <td > <Card id="10_heart.png" textValue='Ten' /></td>
-                                    <td > <Card id="10_spade.png" textValue='Ten'/></td>
-                                    <td > <Card id="10_club.png" textValue='Ten'/></td>
-                                    <td > <Card id="10_diamond.png" textValue='Ten'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="9_heart.png" textValue='Nine' /></td>
-                                    <td > <Card id="9_spade.png" textValue='Nine'/></td>
-                                    <td > <Card id="9_club.png" textValue='Nine'/></td>
-                                    <td > <Card id="9_diamond.png" textValue='Nine'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="8_heart.png" textValue='Eight' /></td>
-                                    <td > <Card id="8_spade.png" textValue='Eight'/></td>
-                                    <td > <Card id="8_club.png"textValue='Eight'/></td>
-                                    <td > <Card id="8_diamond.png" textValue='Eight'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="7_heart.png" textValue='Seven' /></td>
-                                    <td > <Card id="7_spade.png" textValue='Seven'/></td>
-                                    <td > <Card id="7_club.png" textValue='Seven'/></td>
-                                    <td > <Card id="7_diamond.png" textValue='Seven'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="6_heart.png" textValue='Six' /></td>
-                                    <td > <Card id="6_spade.png" textValue='Six'/></td>
-                                    <td > <Card id="6_club.png" textValue='Six'/></td>
-                                    <td > <Card id="6_diamond.png" textValue='Six'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="5_heart.png" textValue='Five' /></td>
-                                    <td > <Card id="5_spade.png" textValue='Five'/></td>
-                                    <td > <Card id="5_club.png" textValue='Five'/></td>
-                                    <td > <Card id="5_diamond.png" textValue='Five'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="4_heart.png" textValue='Four' /></td>
-                                    <td > <Card id="4_spade.png" textValue='Four'/></td>
-                                    <td > <Card id="4_club.png" textValue='Four'/></td>
-                                    <td > <Card id="4_diamond.png" textValue='Four'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="3_heart.png" textValue='Three' /></td>
-                                    <td > <Card id="3_spade.png" textValue='Three'/></td>
-                                    <td > <Card id="3_club.png" textValue='Three'/></td>
-                                    <td > <Card id="3_diamond.png" textValue='Three'/></td>
-
-                                </tr>
-                                <tr>
-                                    <td > <Card id="2_heart.png" textValue='Deuce' /></td>
-                                    <td > <Card id="2_spade.png" textValue='Deuce'/></td>
-                                    <td > <Card id="2_club.png" textValue='Deuce'/></td>
-                                    <td > <Card id="2_diamond.png" textValue='Deuce'/></td>
+                                                </td>
+                                            )
+                                        } )
+                                    }
 
                                 </tr>
                                 </tbody>
