22baae6a66b65ad8e18e62ef1b9a272340f675fb
Pablo <paul.ladiv123@gmail.com>
2018-09-01 19:34:43 +0100

    display relevant decision in PlayerRange

--- src/components/PlayerRange.js
+++ src/components/PlayerRange.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react'
 
+
 class PlayerRange extends Component {
+
+
+    displayDecision = ( decisionId) => {
+        if (decisionId.includes('oop')) {
+            const res = decisionId.slice(0,-4);
+            return res.toUpperCase()
+        }
+        if (decisionId.includes('ip')) {
+            const res = decisionId.slice(0,-3);
+            return res.toUpperCase()
+        }
+    }
 
     render() {
         return(
@@ -10,23 +23,23 @@ class PlayerRange extends Component {
                     <button> {`Enter ${this.props.position.toUpperCase()} range`} </button>
                 </div>
                 <div id='player-decision' className='flex-player-decision'>
-                    <div className='flex-player-decision-item'>
-                        Flop
+                    <div className='flex-player-decision-item header-title-streets'>
+                        FLOP
                     </div>
-                    <div className='flex-player-decision-item'>
-                        {this.props.flop}
+                    <div className='flex-player-decision-item display-player-decision'>
+                        {this.displayDecision(`${this.props.flop}`)}
                     </div>
-                    <div className='flex-player-decision-item'>
-                        Turn
+                    <div className='flex-player-decision-item header-title-streets '>
+                        TURN
                     </div>
-                    <div className='flex-player-decision-item'>
-                        {this.props.turn}
+                    <div className='flex-player-decision-item display-player-decision'>
+                        {this.displayDecision(`${this.props.turn}`)}
                     </div>
-                    <div className='flex-player-decision-item'>
-                        River
+                    <div className='flex-player-decision-item header-title-streets '>
+                        RIVER
                     </div>
-                    <div className='flex-player-decision-item'>
-                        {this.props.river}
+                    <div className='flex-player-decision-item display-player-decision'>
+                        {this.displayDecision(`${this.props.river}`)}
                     </div>
                 </div>
 
