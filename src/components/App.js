c4d418bbf96d0f1fffd4a23ee93b04c3db4d1db5
Pablo <paul.ladiv123@gmail.com>
2018-09-08 23:29:21 +0100

    add button to see previous hands history

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ import PreviousHands from './PreviousHands'
 
 const DEFAULT_MESSAGE =  'SELECT THREE FLOP CARDS'
 
+const ButtonRoute = ({label, handleClick}) => {
+    return (
+        <button onClick={handleClick}> {label} </button>
+    )
+}
+
 class App extends Component {
 
     state = {
@@ -19,7 +25,18 @@ class App extends Component {
         turnToPlay : 'oop',
         toggleCount : 0,
         turnCardYellow : true,
-        displayInstruction: DEFAULT_MESSAGE
+        displayInstruction: DEFAULT_MESSAGE,
+        showPreviousHands: false
+    }
+
+    handleClickPreviousHands = () => {
+        this.state.showPreviousHands ?
+        this.setState({
+            showPreviousHands: false
+        })
+            :  this.setState({
+                showPreviousHands: true
+            })
     }
 
 
@@ -28,19 +45,39 @@ class App extends Component {
     // SELECT CARDS ON THE CHART END
 
   render() {
+
       return (
       <div>
+          {
+              this.state.showPreviousHands ?
+                  <div>
+                      <PreviousHands/>
+                      <ButtonRoute label='GO BACK TO MAIN PAGE'
+                                   handleClick={this.handleClickPreviousHands}
+                      />
+                  </div>
+                  :
+                  <div>
+                      <Main
+                      selectedCards={this.state.selectedCards}
+                      GameStreet={this.state.GameStreet}
+                      playFlow = {this.state.playFlow}
+                      turnToPlay = {this.state.turnToPlay }
+                      toggleCount = {this.state.toggleCount}
+                      turnCardYellow = {this.state.turnCardYellow }
+                      displayInstruction = {this.state.displayInstruction}
+                      />
+                  <ButtonRoute
+                      label='see previous hands'
+                      handleClick={this.handleClickPreviousHands}
+                  />
+                  </div>
 
-       <Main
-           selectedCards={this.state.selectedCards}
-           GameStreet={this.state.GameStreet}
-           playFlow = {this.state.playFlow}
-           turnToPlay = {this.state.turnToPlay }
-           toggleCount = {this.state.toggleCount}
-           turnCardYellow = {this.state.turnCardYellow }
-           displayInstruction = {this.state.displayInstruction}
-       />
-          <PreviousHands/>
+          }
+
+
+
+
 
 
       </div>
